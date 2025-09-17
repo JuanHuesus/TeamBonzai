2584b5ea7a72fd9cbcceba27c06126f72d1dfcee
Topi Vuori <topitvuori@gmail.com>
2025-09-17 15:19:13 +0300

    url fix

--- src/main.tsx
+++ src/main.tsx
@@ -11,23 +11,30 @@ import { AuthProvider } from "./authContext";
 async function enableMocks() {
   if (import.meta.env.VITE_API_BASE_URL === "/mock") {
     const { worker } = await import("./mocks/browser");
-    await worker.start();
+    await worker.start({
+      serviceWorker: { url: `${import.meta.env.BASE_URL}mockServiceWorker.js` },
+    });
   }
 }
 await enableMocks();
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        { index: true, element: <ServicesList /> },
+        { path: "edit/:id", element: <ServiceEdit /> },
+        { path: "new", element: <ServiceEdit /> },
+        { path: "login", element: <Login /> },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <AppLayout />,
-    children: [
-      { index: true, element: <ServicesList /> },
-      { path: "edit/:id", element: <ServiceEdit /> },
-      { path: "new", element: <ServiceEdit /> },
-      { path: "login", element: <Login /> },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL, // "/novi/"
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
